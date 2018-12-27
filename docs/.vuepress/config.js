dbb5fdf9e966163a02f83ea7454f80263618446e
Tommywt <wangtao0907@gmail.com>
2018-12-27 18:18:38 +0800

    完成官网开发

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  base:'/web-ui/',
+  base:'/web-vui/',
   title: 'web-vui',
   description: '基于vue的组件库',
-  head: [
-    ['link', { rel: 'icon', href: './public/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
-  ],
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+  ],
   themeConfig: {
     nav: [
       { text: '主页', link: '/' },
