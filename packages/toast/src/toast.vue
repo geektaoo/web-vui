ebd0e90f9bc4af3aee3cc2034df38d8ae0990cb1
Tommywt <wangtao0907@gmail.com>
2019-03-27 18:06:42 +0800

    给toast添加position属性

--- packages/toast/src/toast.vue
+++ packages/toast/src/toast.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="fate">
-    <div class="v-toast">
+    <div class="v-toast" :class="positionClass">
       <slot></slot>
       <span class="close-btn" v-if="showClose" @click="onClickClose">
         {{showClose.text}}
@@ -12,39 +12,51 @@
 <script>
   export default {
     name: 'VToast',
-    props:{
-      autoClose:{
-        type:Boolean,
-        default:false
+    props: {
+      autoClose: {
+        type: Boolean,
+        default: false
       },
-      duration:{
+      duration: {
         type: Number,
         default: 4000
       },
-      showClose:{
-        type:Object,
-        default(){
+      position: {
+        type: String,
+        default: 'top',
+        validate(value) {
+          return ['top', 'right', 'left', 'middle'].includes(value)
+        }
+      },
+      showClose: {
+        type: Object,
+        default() {
           return {
-            text:'关闭',callback:undefined
+            text: '关闭', callback: undefined
           }
         }
       }
     },
-    mounted() {
-      if (this.autoClose) {
-        setTimeout(()=>{
-          this.close()
-        },this.duration)
+    computed: {
+      positionClass() {
+        return `position-${this.position}`
       }
     },
-    methods:{
-      close(){
+    mounted() {
+      if (this.autoClose) {
+        setTimeout(() => {
+          this.close()
+        }, this.duration)
+      }
+    },
+    methods: {
+      close() {
         this.$el.remove()
         this.$destroy()
       },
-      onClickClose(){
+      onClickClose() {
         this.close()
-        if (this.showClose && typeof this.showClose.callback === 'function'){
+        if (this.showClose && typeof this.showClose.callback === 'function') {
           this.showClose.callback()
         }
       }
@@ -54,6 +66,7 @@
 
 <style lang="scss" scoped>
   @import "../../global";
+
   $content-font-size: 14px;
   $close-btn-size: 10px;
   $position-offset: 8px;
@@ -65,16 +78,14 @@
     display: flex;
     align-items: center;
     position: fixed;
-    top: 10px;
-    left: 50%;
-    transform: translateX(-50%);
     box-shadow: 0 2px 12px #a8abaf;
     color: $font-color;
     background: $toast-bg;
     font-size: $content-font-size;
     padding: $toast-padding;
     border-radius: $border-radius-middle;
-    .close-btn{
+
+    .close-btn {
       margin-left: 16px;
       padding-left: 8px;
       cursor: pointer;
@@ -83,8 +94,36 @@
     }
   }
 
+  .position-top {
+    top: 10px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .position-bottom {
+    bottom: 10px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .position-left {
+    left: 20px;
+    top: 30px;
+  }
+
+  .position-right {
+    right: 20px;
+    top: 30px;
+  }
+
+  .position-middle {
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
   .fate-enter-active {
-    transition: opacity .3s,transform .3s,left .3s,right .3s,top .3s,bottom .3s;
+    transition: opacity .3s, transform .3s, left .3s, right .3s, top .3s, bottom .3s;
   }
 
   .fate-leave-active {
