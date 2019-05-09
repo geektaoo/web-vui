524923f2312790fc2b851fdf4d6be329fe97d5b0
Tommywt <wangtao0907@gmail.com>
2019-05-09 16:40:21 +0800

    修复arrayFormat数据出现0和-1的问题，还有添加前后按钮

--- packages/pagination/src/pagination.vue
+++ packages/pagination/src/pagination.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="v-pagination">
-    <span class="v-pagination-item"
-          v-for="page in pages"
-          :class="{active:currentPage === page,ellipsis:page==='...'}">
-      {{page}}
+    <span class="v-pagination-prve" :class="{disabled:currentPage === 1}" :click="onClickPrve">
+      <v-icon name="left"></v-icon>
+    </span>
+    <template class="" v-for="page in pages">
+      <template v-if="page === currentPage">
+        <span class="v-pagination-item current">{{page}}</span>
+      </template>
+      <template v-else-if="page === '...'">
+        <span class="v-pagination-item ellipsis">
+          <v-icon name="dots"></v-icon>
+        </span>
+      </template>
+      <template v-else>
+        <span class="v-pagination-item">{{page}}</span>
+      </template>
+    </template>
+    <span class="v-pagination-next" :class="{disabled: currentPage===totalPage}" :click="onClickNext">
+      <v-icon name="right"></v-icon>
     </span>
   </div>
 </template>
 
 <script>
+  import vIcon from '../../icon/index'
+
   export default {
     name: 'vPagination',
+    components: {vIcon},
     props: {
       totalPage: {
         type: Number,
@@ -27,9 +44,10 @@
     },
     computed: {
       pages() {
-        //Array Format 页码显示，第一页，最后一页，当前页，当前-1，当前+1
-        let arrayFormat = [1, this.totalPage, this.currentPage, this.currentPage - 1, this.currentPage + 1]
+        //Array Format 页码显示，第一页，最后一页，当前页，当前-1，当前-2,当前+2,当前+1
+        let arrayFormat = [1, this.totalPage, this.currentPage, this.currentPage - 1, this.currentPage + 2, this.currentPage + 1, this.currentPage + 2]
         //对Array Format排序,去重
+        arrayFormat = arrayFormat.filter((n) => n >= 1 && n <= this.totalPage)
         let array = this.unique(arrayFormat.sort((a, b) => a - b))
         let pages = array.reduce((prev, current, index, array) => {
           prev.push(current)
@@ -51,7 +69,11 @@
         return Object.keys(obj).map((str) => {
           return JSON.parse(str)
         })
-      }
+      },
+      onClickPrve() {
+      },
+      onClickNext() {
+      },
     }
   }
 </script>
@@ -60,6 +82,11 @@
   @import "../../../style/var";
 
   .v-pagination {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    user-select: none;
+
     &-item {
       display: inline-flex;
       justify-content: center;
@@ -73,19 +100,46 @@
       color: #bdc3c7;
       cursor: pointer;
 
-      &.active {
+      &.current {
         color: #55efc4;
         background: #dff9fb;
         cursor: default;
       }
 
-      &.ellipsis{
+      &.ellipsis {
         cursor: default;
+        fill: #bdc3c7;
       }
 
       &:hover {
         border-color: #dff9fb;
         color: #55efc4;
+      }
+    }
+
+    &-prve, &-next {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-width: 40px;
+      min-height: 36px;
+      padding: 4px;
+      border-radius: $border-radius;
+      border: 1px solid #eee;
+      cursor: pointer;
+      fill: #95a5a6;
+
+      &:hover {
+        border-color: #dff9fb;
+        fill: #55efc4;
+      }
+
+      &.disabled {
+        cursor: default;
+        background: #ecf0f1;
+        svg{
+          fill:#bdc3c7;
+        }
       }
     }
   }
