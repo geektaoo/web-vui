815dd31bfd67a1627763ee737530c654615ea3b3
Tommywt <wangtao0907@gmail.com>
2019-05-14 00:08:28 +0800

    完成datePicker组件的基本功能

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-date-picker></v-date-picker>
+    <v-date-picker v-model="time"></v-date-picker>
   </div>
 </template>
 
@@ -8,7 +8,9 @@
   export default {
     name: 'app',
     data() {
-      return {}
+      return {
+        time:new Date()
+      }
     },
     mounted() {
     },
@@ -20,13 +22,13 @@
   @import "../style/var.scss";
 
   #app {
-    margin-top: 100px;
+    margin-top: 20px;
     color: #2c3e50;
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     text-align: center;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    display: flex;
-    justify-content: center;
+    /*display: flex;*/
+    /*justify-content: center;*/
   }
 </style>
--- packages/datePicker/src/datePicker.vue
+++ packages/datePicker/src/datePicker.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="v-datePicker">
-    <input type="text" class="input">
+    <input type="text" class="input" :value="dateValue" @click="openPanel">
     <transition name="fadeDownBig">
-      <div class="date-panel">
+      <div class="date-panel" v-show="panelState">
         <!--顶部按钮以及年月显示条-->
         <div class="topbar">
-          <span><v-icon name="left"></v-icon></span>
-          <span class="year">2019</span>
-          <span class="month">3</span>
-          <span><v-icon name="right"></v-icon></span>
+          <span @click="onPre"><v-icon name="left"></v-icon></span>
+          <span class="year">{{tmpYear}}</span>
+          <span class="month">{{changeTmpMonth}}</span>
+          <span @click="onNext"><v-icon name="right"></v-icon></span>
         </div>
         <!-- 日期面板-->
-        <div class="date-group">
-          <span class="weekday"></span>
+        <div class="date-group" v-show="panelType === 'date'">
+          <span class="weekday" v-for="(item,index) in weekList" :key="index">{{item.label}}</span>
           <ul class="date-list">
-            <li></li>
+            <li v-for="(item,index) in dateList"
+                v-text="item.value"
+                :key="index"
+                :class="{
+                preMonth:item.previousMonth,
+                nextMonth:item.nextMonth,
+                selected: date === item.value && month === tmpMonth && item.currentMonth, invalid: validateDate(item)
+                }"
+                @click="selectDate(item)"
+            >
+            </li>
           </ul>
         </div>
       </div>
@@ -28,13 +38,179 @@
   export default {
     name: 'vDatePicker',
     components: {vIcon},
+    props: {
+      value: {
+        type: [Date, String],
+        default: ''
+      },
+      format: {
+        type: String,
+        default: 'yyyy-MM-dd'
+      }
+    },
     data() {
       return {
-        dateValue: ''//输入框显示日期
+        dateValue: '',//输入框显示日期
+        date: new Date().getDate(),//当前日期
+        panelState: false,//初始值。默认panel关闭
+        tmpMonth: new Date().getMonth(), // 临时月份，可修改
+        month: new Date().getMonth(),
+        tmpYear: new Date().getFullYear(), // 临时年份，可修改
+        weekList: [
+          {label: '日', value: 0},
+          {label: '一', value: 1},
+          {label: '二', value: 2},
+          {label: '三', value: 3},
+          {label: '四', value: 4},
+          {label: '五', value: 5},
+          {label: '六', value: 6}
+        ], // 周
+        monthList: [
+          {label: '1月', value: 0},
+          {label: '2月', value: 1},
+          {label: '3月', value: 2},
+          {label: '4月', value: 3},
+          {label: '5月', value: 4},
+          {label: '6月', value: 5},
+          {label: '7月', value: 6},
+          {label: '8月', value: 7},
+          {label: '9月', value: 8},
+          {label: '10月', value: 9},
+          {label: '11月', value: 10},
+          {label: '12月', value: 11}
+        ], // 月
+        nowValue: 0, // 当前选中日期值
+        panelType: 'date' // 面板状态
+      }
+    },
+    mounted() {
+      // console.log(this.dateValue, 'date')
+      if (this.value) {
+        // console.log(this.value, 'value')
+        this.dateValue = this.formatDate(new Date(this.value).getTime())
+      }
+    },
+    computed: {
+      dateList() {
+        //获取当月的天数
+        let currentMonthLength = new Date(this.tmpYear, this.tmpMonth + 1, 0).getDate()
+        // console.log(currentMonthLength, 'current对象')
+        let dateList = Array.from({length: currentMonthLength}, (val, index) => {
+            return {
+              currentMonth: true,
+              value: index + 1
+            }
+          }
+        )
+        //获取当月1号的星期是为了确定1号前需要插多少天
+        let startDay = new Date(this.tmpYear, this.tmpMonth, 1).getDay()
+        //确认上个月一共多少天
+        let previousMongthLength = new Date(
+          this.tmpYear,
+          this.tmpMonth,
+          0
+        ).getDate()
+        // 在1号前插入上个月日期
+        for (let i = 0, len = startDay; i < len; i++) {
+          dateList = [
+            {previousMonth: true, value: previousMongthLength - i}
+          ].concat(dateList)
+        }
+        // 补全剩余位置,至少14天，则 i < 15
+        for (let i = 1, item = 1; i < 15; i++, item++) {
+          dateList[dateList.length] = {nextMonth: true, value: i}
+        }
+        // console.log(dateList, 'dateList')
+        return dateList
+      },
+      changeTmpMonth() {
+        return this.monthList[this.tmpMonth].label
+      },
+      // 通过改变this.tmpYear则可以改变年份数组
+      yearList() {
+        return Array.from({length: 12}, (value, index) => this.tmpYear + index)
       }
     },
     methods: {
       openPanel() {
+        this.panelState = !this.panelState
+        this.panelType = 'date'
+      },
+      selectDate(item) {
+        // 赋值 当前 nowValue,用于控制样式突出显示当前月份日期
+        this.nowValue = item.value
+        // 选择了上个月
+        if (item.previousMonth) {
+          this.tmpMonth--
+          if (this.tmpMonth < 0) {
+            this.tmpMonth = 11
+            this.tmpYear = this.tmpYear - 1
+          }
+        }
+        // 选择了下个月
+        if (item.nextMonth) {
+          this.tmpMonth++
+          if (this.tmpMonth > 11) {
+            this.tmpMonth = 0
+            this.tmpYear = this.tmpYear + 1
+          }
+        }
+        // 获取选中日期的 date
+        let selectDay = new Date(this.tmpYear, this.tmpMonth, this.nowValue)
+        // 格式日期为字符串后，赋值给 input
+        this.dateValue = this.formatDate(selectDay.getTime())
+        // 关闭面板
+        this.panelState = !this.panelState
+        this.$emit('input', selectDay)
+      },
+// 日期格式方法
+      formatDate(date, fmt = this.format) {
+        if (date === null || date === 'null') {
+          return '--'
+        }
+        date = new Date(Number(date))
+        var o = {
+          'M+': date.getMonth() + 1, // 月份
+          'd+': date.getDate(), // 日
+          'h+': date.getHours(), // 小时
+          'm+': date.getMinutes(), // 分
+          's+': date.getSeconds(), // 秒
+          'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
+          S: date.getMilliseconds() // 毫秒
+        }
+        if (/(y+)/.test(fmt))
+          fmt = fmt.replace(
+            RegExp.$1,
+            (date.getFullYear() + '').substr(4 - RegExp.$1.length)
+          )
+        for (var k in o) {
+          if (new RegExp('(' + k + ')').test(fmt))
+            fmt = fmt.replace(
+              RegExp.$1,
+              RegExp.$1.length === 1
+                ? o[k]
+                : ('00' + o[k]).substr(('' + o[k]).length)
+            )
+        }
+        return fmt
+      },
+// 确认是否为当前月份
+      validateDate(item) {
+        if (this.nowValue === item.value && item.currentMonth) return true
+      },
+      onPre() {
+        this.tmpMonth--
+        if (this.tmpMonth < 0) {
+          this.tmpMonth = 11
+          this.tmpYear = this.tmpYear - 1
+        }
+      },
+      onNext() {
+        this.tmpMonth++
+        if (this.tmpMonth > 11) {
+          this.tmpMonth = 0
+          this.tmpYear = this.tmpYear + 1
+        }
       }
     }
   }
@@ -44,6 +220,7 @@
   .topbar {
     padding-top: 8px;
   }
+
   .topbar span {
     display: inline-block;
     width: 20px;
@@ -52,22 +229,27 @@
     color: #515a6e;
     cursor: pointer;
   }
+
   .topbar span:hover {
     color: #2d8cf0;
   }
+
   .topbar .year,
   .topbar .month {
     width: 60px;
   }
+
   .year-list {
     height: 200px;
     width: 210px;
   }
+
   .year-list .selected {
     background: #2d8cf0;
     border-radius: 4px;
     color: #fff;
   }
+
   .year-list li {
     display: inline-block;
     width: 70px;
@@ -76,14 +258,17 @@
     border-radius: 10px;
     cursor: pointer;
   }
+
   .year-list span {
     display: inline-block;
     line-height: 16px;
     padding: 8px;
   }
+
   .year-list span:hover {
     background: #e1f0fe;
   }
+
   .weekday {
     display: inline-block;
     font-size: 13px;
@@ -91,21 +276,25 @@
     color: #c5c8ce;
     text-align: center;
   }
+
   .v-datePicker {
     width: 210px;
     text-align: center;
     font-family: "Avenir", Helvetica, Arial, sans-serif;
   }
+
   .date-panel {
     width: 210px;
     box-shadow: 0 0 8px #ccc;
     background: #fff;
   }
+
   ul {
     list-style: none;
     padding: 0;
     margin: 0;
   }
+
   .date-list {
     width: 210px;
     text-align: left;
@@ -113,6 +302,7 @@
     overflow: hidden;
     margin-top: 4px;
   }
+
   .date-list li {
     display: inline-block;
     width: 28px;
@@ -124,20 +314,25 @@
     border: 1px solid #fff;
     border-radius: 4px;
   }
+
   .date-list .selected {
     border: 1px solid #2d8cf0;
   }
+
   .date-list .invalid {
     background: #2d8cf0;
     color: #fff;
   }
+
   .date-list .preMonth,
   .date-list .nextMonth {
     color: #c5c8ce;
   }
+
   .date-list li:hover {
     background: #e1f0fe;
   }
+
   .input {
     display: inline-block;
     box-sizing: border-box;
@@ -157,6 +352,7 @@
     box-shadow 0.2s ease-in-out;
     margin-bottom: 6px;
   }
+
   .fadeDownBig-enter-active,
   .fadeDownBig-leave-active,
   .fadeInDownBig {
@@ -165,14 +361,17 @@
     -webkit-animation-fill-mode: both;
     animation-fill-mode: both;
   }
+
   .fadeDownBig-enter-active {
     -webkit-animation-name: fadeInDownBig;
     animation-name: fadeInDownBig;
   }
+
   .fadeDownBig-leave-active {
     -webkit-animation-name: fadeOutDownBig;
     animation-name: fadeOutDownBig;
   }
+
   @-webkit-keyframes fadeInDownBig {
     from {
       opacity: 0.8;
@@ -185,6 +384,7 @@
       transform: none;
     }
   }
+
   @keyframes fadeInDownBig {
     from {
       opacity: 0.8;
@@ -197,6 +397,7 @@
       transform: none;
     }
   }
+
   @-webkit-keyframes fadeOutDownBig {
     from {
       opacity: 1;
@@ -207,6 +408,7 @@
       transform: translate3d(0, -4px, 0);
     }
   }
+
   @keyframes fadeOutDownBig {
     from {
       opacity: 1;
